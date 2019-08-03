625c7a12563c6645cb14810bf97ea79894b66ecb
serelath <shoh@go.pasadena.edu>
2019-08-02 18:09:57 -0700

    image3

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,12 @@
-new hoverEffect({
+var button = new hoverEffect({
     parent: document.querySelector('.distortion'),
     intensity: 0.2,
     image1: "assets/images/max-olivo.jpg",
     image2: "assets/images/max-olivo.jpg",
+    image3: "assets/images/max-olivo.jpg",
     displacementImage: "assets/images/heightMap.png"
 });
+
+
+document.querySelector('.next').addEventListener('click', button.next);
+document.querySelector('.previous').addEventListener('click', button.previous)
