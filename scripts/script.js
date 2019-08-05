c138ae89b8292e1afe3c9082c60fd21b51e798fc
serelath <shoh@go.pasadena.edu>
2019-08-04 18:23:32 -0700

    added cursor

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,8 @@
 new hoverEffect({
     parent: document.querySelector('.max-portrait'),
-    intensity: 0.2,
+    intensity: 0.5,
+    easing: 'Power1.easeOut',
+    angle: 2,
     image1: 'assets/images/1.jpeg',
     image2: 'assets/images/1.jpeg',
     displacementImage: 'assets/images/heightMap.png'
